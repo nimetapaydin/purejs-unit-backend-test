cb48b328c0d65f5cf87f874eed8a8ab9a35a72df
nimetapaydin <nimetapaydin@gmail.com>
2022-04-02 23:48:31 +0300

    repository.test.js updated

--- repository.test.js
+++ repository.test.js
@@ -2,9 +2,10 @@
 // insert metodu ile veri ekler remove metodu ile todo yu siler
 
 import Repository from "./repository";
-describe ("Veritabanı servi", ()=> {
+describe ("Veritabanı servi", (todo)=> {
     test("Geçersiz sorgular hata vermelidir", () => {
-      
+        if (!todo || !todo.todo || typeof(todo.todo) !== "string" || todo.todo.trim() == "")
+        return "400";
         
     })
 })
